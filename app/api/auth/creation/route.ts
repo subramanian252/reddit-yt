6bbb7d6d2bb5bfbf3767987ec2ca52f033c8fc18
Surya Subramanian <suryasubramanian252@gmail.com>
2024-07-27 01:39:16 +0530

    second commit

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -33,5 +33,9 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect("http://localhost:3000/");
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/"
+      : "https://reddit-yt-seven.vercel.app/"
+  );
 }
